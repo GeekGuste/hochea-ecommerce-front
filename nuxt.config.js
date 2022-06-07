b0fbcf8600c63b7be8b3aedfb76ed7ccfd539f9c
GeekGuste <36772043+GeekGuste@users.noreply.github.com>
2022-06-07 19:43:03 +0200

    update api url

--- nuxt.config.js
+++ nuxt.config.js
@@ -109,7 +109,7 @@ export default {
     // Axios module configuration: https://go.nuxtjs.dev/config-axios
     axios: {
         // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-        baseURL: process.env.BASE_URL || 'http://127.0.0.1:8000'
+        baseURL: process.env.BASE_URL || 'https://hochea.tincom.biz'
     },
 
     // Build Configuration: https://go.nuxtjs.dev/config-build
