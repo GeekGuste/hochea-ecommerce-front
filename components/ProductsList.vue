6d6b77043dd86a6bdabc6f051399cf7571a42817
GeekGuste <36772043+GeekGuste@users.noreply.github.com>
2023-04-10 22:52:07 +0200

    return 12 last product at homepage

--- components/ProductsList.vue
+++ components/ProductsList.vue
@@ -1,78 +1,140 @@
 <template>
-<div>
-  <b-row class="d-flex justify-content-center">
-    <template v-if="!!products && products.length>0">
-      <div
-        class="card m-2 mb-4 shadow-sm"
-        v-for="product in products"
-        :key="product.id"
-      >
-        <div class="card-img">
-          <img
-            :src="product.principal_image"
-            class="card-img-top img-fluid product-image"
-            :alt="product.label"
-          />
-        </div>
-        <div class="card-body d-flex flex-column justify-content-between">
-          <div>
-            <h4 class="card-title mb-3">{{ product.label }}</h4>
-            <div v-if="product.categories.length > 0">
+  <div>
+    <b-row class="d-flex justify-content-center">
+      <template v-if="!!products && products.length > 0">
+        <div
+          class="card m-2 mb-4 shadow-sm"
+          v-for="product in products"
+          :key="product.id"
+        >
+          <div class="card-img">
+            <img
+              :src="product.principal_image"
+              class="card-img-top img-fluid product-image"
+              :alt="product.label"
+            />
+          </div>
+          <div class="card-body d-flex flex-column justify-content-between">
+            <div>
+              <h4 class="card-title mb-3">{{ product.label }}</h4>
+              <div v-if="product.categories.length > 0">
+                <p class="my-2">
+                  <span
+                    class="badge badge-primary text-capitalize p-2 m-1"
+                    v-for="category of product.categories"
+                    :key="category.id"
+                  >
+                    <NuxtLink
+                      class="product-category"
+                      :to="`/search?category=${category.id}`"
+                    >
+                      {{ category.label }}
+                    </NuxtLink>
+                  </span>
+                </p>
+              </div>
               <p class="my-2">
-                <span class="badge badge-primary text-capitalize p-2 m-1" v-for="category of product.categories" :key="category.id">
-                  <NuxtLink class="product-category" :to="`/search?category=${category.id}`">
-                    {{ category.label }}
-                  </NuxtLink>
+                <span class="text-muted">Price: </span>
+                <span v-if="!!product.promo_price" class="text-capitalize">
+                  <b
+                    ><strike>{{ product.price }} €</strike></b
+                  >
+                  &nbsp;&nbsp;
+                  <b>{{ product.promo_price }} €</b>
+                </span>
+                <span v-else class="text-capitalize">
+                  <b>{{ product.price }} €</b>
                 </span>
               </p>
             </div>
-            <p class="my-2">
-              <span class="text-muted">Price: </span>
-              <span v-if="!!product.promo_price" class="text-capitalize">
-                <b
-                  ><strike>{{ product.price }} €</strike></b
-                >
-                &nbsp;&nbsp;
-                <b>{{ product.promo_price }} €</b>
-              </span>
-              <span v-else class="text-capitalize">
-                <b>{{ product.price }} €</b>
-              </span>
-            </p>
-          </div>
-          <div class="d-inline-block mt-4">
-            <NuxtLink :to="'/product/' + product.id" class="text-decoration-none">
-              <button
-                class="
-                  btn btn-warning btn-sm
-                  w-100
-                  d-flex
-                  align-items-center
-                  justify-content-center
-                  text-white
-                "
+            <div class="d-inline-block mt-4">
+              <NuxtLink
+                :to="'/product/' + product.id"
+                class="text-decoration-none"
               >
-                Détails
-                <i class="fas fa-angle-double-right mx-1"></i>
-              </button>
-            </NuxtLink>
+                <button
+                  class="btn btn-warning btn-sm w-100 d-flex align-items-center justify-content-center text-white"
+                >
+                  Détails
+                  <i class="fas fa-angle-double-right mx-1"></i>
+                </button>
+              </NuxtLink>
+            </div>
           </div>
         </div>
-      </div>
-    </template>
-    <template v-else>
-      <div class="alert alert-danger">
+      </template>
+      <template v-else>
+        <div class="alert alert-danger">
           <center>Aucun produit ne correspond à votre recherche</center>
+        </div>
+      </template>
+    </b-row>
+    <b-row
+      class="d-flex justify-content-center"
+      v-if="!!productsList.next || !!productsList.previous"
+    >
+      <div>
+        <b-pagination-nav
+          :link-gen="linkGen"
+          v-model="currentPage"
+          v-bind:number-of-pages="numberOfPages"
+        ></b-pagination-nav>
       </div>
-    </template>
-  </b-row>
-  <b-row class="d-flex justify-content-center" v-if="!!productsList.next || !!productsList.previous">
-    <div>
-      <b-pagination-nav :link-gen="linkGen" v-model="currentPage" v-bind:number-of-pages="numberOfPages"></b-pagination-nav>
-    </div>
-  </b-row>
-</div>
+    </b-row>
+  </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+import { PaginatedList } from '../models/pagination'
+import { Product } from '../models/product'
+export default Vue.extend({
+  name: 'ProductsList',
+  props: {
+    productsList: null
+  },
+  data() {
+    return {
+      //products: [],
+      currentPage: 1
+    }
+  },
+  mounted() {
+    this.currentPage = this.$route.query.page || 1
+  },
+  watch: {
+    '$route.query'() {
+      this.currentPage = this.$route.query.page || 1
+    }
+  },
+  computed: {
+    products() {
+      return this.productsList?.results
+    },
+    numberOfPages() {
+      return Math.ceil(this.productsList?.count / 12)
+    }
+  },
+  methods: {
+    productUrl(product: Product) {
+      return `/product/${product.id}`
+    },
+    linkGen(pageNum: Number) {
+      let query = this.$route.query
+      query.page = pageNum
+      return this.$router.history.current.path + '?' + this.toQueryString(query)
+    },
+    toQueryString(obj: Object) {
+      let str = []
+      for (var p in obj) {
+        if (obj.hasOwnProperty(p)) {
+          str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]))
+        }
+      }
+      return str.join('&')
+    }
+  }
+})
+</script>
 <style lang="css" scoped>
 .card {
   width: 21.25rem;
@@ -87,60 +149,8 @@ img {
   transform: scale(0.75);
   object-fit: contain;
 }
-.product-category{
+.product-category {
   color: #fff;
   text-decoration: none;
 }
 </style>
-<script lang="ts">
-import Vue from "vue";
-import { PaginatedList } from "../models/pagination";
-import { Product } from "../models/product";
-export default Vue.extend({
-  name: "ProductsList",
-  props: {
-    productsList: null,
-  },
-  data() {
-    return {
-      //products: [],
-      currentPage: 1
-    };
-  },
-  mounted(){
-    this.currentPage = this.$route.query.page || 1;
-  },
-  watch: {
-    "$route.query" (){
-      this.currentPage = this.$route.query.page || 1;
-    }
-  },
-  computed: {
-    products(){
-      return this.productsList?.results;
-    },
-    numberOfPages(){
-      return Math.ceil(this.productsList?.count/12);
-    }
-  },
-  methods: {
-    productUrl(product: Product) {
-      return `/product/${product.id}`;
-    },
-    linkGen(pageNum: Number){
-      let query = this.$route.query;
-      query.page = pageNum;
-      return this.$router.history.current.path + "?" + this.toQueryString(query);
-    },
-    toQueryString(obj: Object){
-      let str = [];
-      for (var p in obj){
-        if (obj.hasOwnProperty(p)) {
-          str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-        }
-      }
-      return str.join("&");
-    }
-  },
-});
-</script>
--- pages/index.vue
+++ pages/index.vue
@@ -6,65 +6,72 @@
     <b-row>
       <b-col md="8" offset-md="2" lg="6" offset-lg="3">
         <b-row class="d-flex justify-content-center">
-          <b-col  v-for="cat in categoryTree" :key="cat.id" md="3" sm="4">
-          <nuxt-link :to="generateCategoryUrl(cat)">
-            <b-img :src="cat.image" fluid :alt="cat.label"></b-img>
-            <center><b>{{ cat.label }}</b></center>
-          </nuxt-link>
-            <br>
+          <b-col v-for="cat in categoryTree" :key="cat.id" md="3" sm="4">
+            <nuxt-link :to="generateCategoryUrl(cat)">
+              <b-img :src="cat.image" fluid :alt="cat.label"></b-img>
+              <center>
+                <b>{{ cat.label }}</b>
+              </center>
+            </nuxt-link>
+            <br />
           </b-col>
         </b-row>
       </b-col>
     </b-row>
     <b-row>
       <b-col sm="12">
-        <h2 v-if="!!productsList && !!productsList.count && productsList.count > 0"><center>Nos derniers produits</center></h2>
+        <h2
+          v-if="
+            !!productsList && !!productsList.length && productsList.length > 0
+          "
+        >
+          <center>Nos derniers produits</center>
+        </h2>
         <ProductsList :productsList="productsList" />
       </b-col>
     </b-row>
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+import SideBar from '../components/widget/SideBar.vue'
+import Slider from '../components/widget/Slider.vue'
+import { CategoryTree } from '../models/category'
+import { PaginatedList } from '../models/pagination'
+import { Product } from '../models/product'
+export default Vue.extend({
+  components: { SideBar, Slider },
+  name: 'IndexPage',
+  data() {
+    return {
+      categoryTree: [],
+      productsList: []
+    }
+  },
+  async mounted() {
+    this.$axios
+      .$get('/api/category/tree/')
+      .then((categoryTree: CategoryTree[]) => {
+        this.categoryTree = categoryTree
+      })
+    // On prend les derniers produits enregistrés pour la page d'accueil
+    this.$axios
+      .$get('/api/product/last/')
+      .then((productsList: PaginatedList<Product>) => {
+        this.productsList = productsList
+      })
+  },
+  methods: {
+    generateCategoryUrl(tree: any) {
+      return `/search?category=${tree.id}`
+    }
+  }
+})
+</script>
+
 <style>
 .filtre-container {
   margin-top: 20px;
 }
 </style>
-
-<script lang="ts">
-import Vue from "vue";
-import SideBar from "../components/widget/SideBar.vue";
-import Slider from "../components/widget/Slider.vue";
-import { CategoryTree } from "../models/category";
-import { PaginatedList } from "../models/pagination";
-import { Product } from "../models/product";
-export default Vue.extend({
-  components: { SideBar, Slider },
-  name: "IndexPage",
-  data() {
-    return {
-      categoryTree: [],
-      productsList: [],
-    };
-  },
-  async mounted() {
-    this.$axios
-      .$get("/api/category/tree/")
-      .then((categoryTree: CategoryTree[]) => {
-        this.categoryTree = categoryTree;
-      });
-    this.$axios
-      .$get("/api/product/", {
-        params: { is_variant: "False", ...this.$route.query },
-      })
-      .then((productsList: PaginatedList<Product>) => {
-        this.productsList = productsList;
-      });
-  },
-  methods:{
-    generateCategoryUrl(tree: any){
-      return `/search?category=${tree.id}`;
-    }
-  }
-});
-</script>
